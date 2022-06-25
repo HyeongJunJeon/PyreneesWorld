962aa61dcbe58ea2ea47f2908fa02757e93859db
전형준 <wehj0105@gamil.com>
2022-06-25 13:20:09 +0900

    초기세팅완료

--- src/App.js
+++
@@ -1,7 +0,0 @@
-import "./App.css";
-
-function App() {
-  return <div>피레니즈 월드! 메인!</div>;
-}
-
-export default App;
---
+++ src/pages/Main/Main.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Main = () => {
+  return <div>피레니즈 월드 메인!</div>;
+};
+
+export default Main;
